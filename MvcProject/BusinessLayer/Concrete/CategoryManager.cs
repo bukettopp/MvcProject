e2efaaf39bbb97c0dc055941b4a54753f42add3a
bukettopp <157303213+bukettopp@users.noreply.github.com>
2024-03-16 05:58:29 +0300

    update

--- MvcProject/BusinessLayer/Concrete/CategoryManager.cs
+++ MvcProject/BusinessLayer/Concrete/CategoryManager.cs
@@ -1,4 +1,6 @@
-﻿using DataAccessLayer.Concrete.Repositories;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Concrete.Abstract;
+using DataAccessLayer.Concrete.Repositories;
 using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
@@ -8,22 +10,18 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class CategoryManager
+    public class CategoryManager : ICategoryService
     {
-        GenericRepository<Category> repo=new();
-        public List<Category> GetAllBL() {
-            return repo.List(); 
-                }
-        public void CategoryAddAlBL(Category p)
+        ICategoryDal _categorydal;
+        public CategoryManager(ICategoryDal categoryDal)
         {
-            if(p.CategoryName=="" ||  p.CategoryName.Length<=3 || p.CategoryDescription=="" || p.CategoryName.Length >=51)
-            {
-              //hata mesajı
-            }
-            else
-            {
-                repo.Insert(p);
-            }
+            _categorydal = categoryDal;
+        }
+
+
+        public List<Category> GetList()
+        {
+            return _categorydal.List();
         }
     }
 }
--- MvcProject/MvcProject.Web/Controllers/CategoryController.cs
+++ MvcProject/MvcProject.Web/Controllers/CategoryController.cs
@@ -1,20 +1,33 @@
 ﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MvcProject.Web.Controllers
 {
     public class CategoryController : Controller
     {
-        CategoryManager cm = new CategoryManager();
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public IActionResult Index()
         {
             return View();
         }
         public ActionResult GetCategoryList()
         {
-            var categoryvalues = cm.GetAllBL();
+            var categoryvalues = cm.GetList();
             return View(categoryvalues);
 
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+          //cm.CategoryAddAlBL(p);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
