e5040d74e472b01d5c90f665f85da74138cded94
bukettopp <157303213+bukettopp@users.noreply.github.com>
2024-06-12 22:47:14 +0300

    Update

--- MvcProject/BusinessLayer/Concrete/ContactManager.cs
+++ MvcProject/BusinessLayer/Concrete/ContactManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,39 @@ using System.Threading.Tasks;
 
 namespace BusinessLayer.Concrete
 {
-    public class ContactManager
+    public class ContactManager : IContactService
     {
+        IContactDal _Contactdal;
+
+
+        public ContactManager(IContactDal contactDal)
+
+        {
+            _Contactdal = contactDal;
+        }
+        public void ContactAdd(Contact contact)
+        {
+            _Contactdal.Insert(contact);
+        }
+
+        public void ContactDelete(Contact contact)
+        {
+            _Contactdal.Delete(contact);
+        }
+
+        public void ContactUpdate(Contact contact)
+        {
+            _Contactdal.Update(contact);
+        }
+
+        public Contact GetByID(int id)
+        {
+            return _Contactdal.Get(x=>x.ContactID==id);
+        }
+
+        public List<Contact> GetList()
+        {
+           return _Contactdal.List();
+        }
     }
 }
--- MvcProject/MvcProject.Web/Controllers/ContactController.cs
+++ MvcProject/MvcProject.Web/Controllers/ContactController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcProject.Web.Controllers
 {
     public class ContactController : Controller
     {
+        ContactManager cm = new(new EfContactDal());
+        ContactValidator cv = new ContactValidator();
+
         public IActionResult Index()
         {
-            return View();
+            var contactvalues = cm.GetList();
+            return View(contactvalues);
         }
     }
 }
