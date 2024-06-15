b81db11ff3ca50c5f1c375782a977b9000c373b5
bukettopp <157303213+bukettopp@users.noreply.github.com>
2024-06-15 23:59:06 +0300

    update

---
+++ MvcProject/MvcProject.Web/Controllers/MessageController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcProject.Web.Controllers
+{
+    public class MessageController : Controller
+    {
+       MessageManager cm = new(new EfMessageDal());
+       // ContactValidator cv = new ContactValidator();
+        public IActionResult Inbox()
+        {
+            var messageList = cm.GetListInbox();
+            return View(messageList);
+        }
+        public ActionResult Sendbox() {
+        
+        var messageList = cm.GetListSendbox();  
+            return View(messageList);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message p)
+        {
+            return View();
+
+        }
+    }
+}
